e4a0a9c81adf6e60ad7916f363715bde24fa547c
khushiv30 <khushi.varshney_cs19@gla.ac.in>
2022-09-07 18:30:34 +0530

    push

---
+++ Day-2/main.js
@@ -0,0 +1,52 @@
+
+const game = new PIXI.Application({
+    width: 800,
+    height: 600,
+    backgroundColor: 0x3366FF,
+    // canvas:document.getElementById('game')
+});
+console.log('hello!', game);
+document.getElementById('game').append(game.view);
+
+loadAssets([
+    { name: 'back', url: '/assets/cardback.png' },
+    // {name:'front',url:'/assets/smilies.png'}
+], start)
+
+
+////////////////////////////////////////////////////////////////
+
+const pBar = document.getElementById('bar');
+const pText = document.getElementById('progress');
+
+function preload(e) {
+    pBar.style.width = e.progress * 2 + "%";
+    pText.innerText = e.progress + "%";
+    if (e.progress === 100) {
+        console.log('hide loader');
+        setTimeout(() => {
+            document.getElementById('loader').style.display = "none";
+        }, 2000);
+        document.getElementById('loader').style.display = 'none';
+    }
+    console.log(e.progress);
+}
+
+function loadAssets(list, onLoadComplete) {
+    game.loader.onProgress.add(preload);
+    game.loader.add(list).load(onLoadComplete);
+}
+
+
+function start(loader, resources) {
+    console.log('params', arguments);
+    const back = PIXI.Sprite.from(resources['back'].texture);
+    back.scale.set(0.2);
+    game.stage.addChild(back);
+
+    const smily = new PIXI.Texture (resources['front'],)
+
+    const front = PIXI.Sprite.from(resources['front'].texture);
+    back.scale.set(0.2);
+    game.stage.addChild(front);
+}
